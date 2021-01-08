bf42d66b0de4e6cbed49540f22b887e21db92052
夏强 <65110691+xq1998@users.noreply.github.com>
2021-01-08 11:53:14 +0800

    test

---
+++ childOne/src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import cn.hutool.core.date.DateUtil;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        String dateStr = "2012-12-12 12:12:12";
+        Date date = DateUtil.parse(dateStr);
+        System.out.println(date);
+        System.out.println( "能运行父类中的hutool jar包，说明是父子关系了" );
+    }
+}
